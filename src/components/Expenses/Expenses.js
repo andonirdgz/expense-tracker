39a4e9b4165f3bee51faaac42f86b6dd8bdaec45
andonirdgz <andoni_rdgz@outlook.com>
2021-06-19 15:47:32 -0500

    Dynamic rendering of ExpenseItems refactor

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,8 +7,6 @@ import "./Expenses.css";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
-  const items = props.items;
-
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
@@ -20,26 +18,13 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={items[0].title}
-          amount={items[0].amount}
-          date={items[0].date}
-        />
-        <ExpenseItem
-          title={items[1].title}
-          amount={items[1].amount}
-          date={items[1].date}
-        />
-        <ExpenseItem
-          title={items[2].title}
-          amount={items[2].amount}
-          date={items[2].date}
-        />
-        <ExpenseItem
-          title={items[3].title}
-          amount={items[3].amount}
-          date={items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
